8c6ec20034c2a676057735c456e38f110e005ef8
Tanyan <43072116+ttanyan@users.noreply.github.com>
2019-08-09 14:36:14 +0800

    perl邮件脚本
    用于在主程序中调用

---
+++ sendDiskMail.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+$to = '1075379406@qq.com';
+$from = 'tanlianwang@jimimax.com';
+$subject = 'The server 172.25.3.198 DISK Alarm';
+$message = 'The server 198(172.25.3.198) DISK alarm, please take action for it. thanks! address:/home/jimi/jimi_aed/DICB/logDetails.txt';
+open(MAIL, "|/usr/sbin/sendmail -t");
+print MAIL "To: $to\n";
+print MAIL "From: $from\n";
+print MAIL "Subject: $subject\n";
+print MAIL "Content-type: text/html\n";
+print MAIL $message;
+close(MAIL);
+print "邮件发送成功\n";
