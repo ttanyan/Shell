c208680f8787cfc198d1ccd1fc1aef0334fb1160
anyant <1075379406@qq.com>
2019-09-12 00:51:20 +0800

    【增加】jdk自动部署脚本

--- DeployScript/JdkDeploy.sh
+++ DeployScript/JdkDeploy.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+#下载jdk1.8
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
+#解压
+tar zxvf jdk-8u141-linux-x64.tar.gz
+#以追加的形式添加环境变量
+#环境生效
+#检查安装
+
+
+
