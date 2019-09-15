d1e2236472a8afa4717f90cd3e268ba348f324cb
anyant <1075379406@qq.com>
2019-09-15 17:55:57 +0800

    【优化】Mysql自动部署脚本

--- DeployScript/JdkDeploy.sh
+++ DeployScript/JdkDeploy.sh
@@ -4,7 +4,7 @@ profile="/opt/middle"
 #环境变量
 environment="/etc/profile"
 #下载jdk1.8.14  不知道为什么 这个无法在脚本上执行下载
-#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
 
 #解压
 tar zxvf jdk-8u141-linux-x64.tar.gz
@@ -19,8 +19,6 @@ echo "export PATH=\$PATH:\$JAVA_HOME/bin" >>${environment}
 source ${environment}
 #检查安装
 java -version
-#环境生效 不知道为什么还需要一次才能生效
-source ${environment}
 
 
 
--- DeployScript/MysqlDeploy.sh
+++ DeployScript/MysqlDeploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #安装目录
-contents="/usr/local/mysql"
+contents="/opt/middle/mysql"
+#contents="/usr/local/mysql"
+
 #配置文件
 myconfig="/etc/my.cnf"
 #环境变量
 environment="/etc/profile"
+#初始化之前先执行
+yum -y install numactl
+yum search libaio
+yum install libaio
 #下载
-#wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.26-linux-glibc2.12-x86_64.tar.gz
+wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.26-linux-glibc2.12-x86_64.tar.gz
 #解压
 tar -xvf mysql-5.7.26-linux-glibc2.12-x86_64.tar.gz
 #移动
@@ -20,12 +26,9 @@ chown -R mysql.mysql ${contents}
 #在mysql目录下创建data目录
 cd  ${contents}
 mkdir data
-#初始化之前先执行
-#yum -y install numactl
-#yum search libaio
-#yum install libaio
+
 #初始化数据
-${contents}/bin/mysql_install_db --user=mysql --basedir=${contents} --datadir=${contents}/data --initialize
+${contents}/bin/mysql_install_db  --no-defaults --user=mysql --basedir=${contents} --datadir=${contents}/data --initialize
 #编辑配置文件 /etc/my.cnf
 #先清空
  >${myconfig}
@@ -33,16 +36,22 @@ ${contents}/bin/mysql_install_db --user=mysql --basedir=${contents} --datadir=${
 echo "[mysqld]" >>${myconfig}
 echo "datadir=${contents}/data" >>${myconfig}
 echo "basedir=${contents}" >>${myconfig}
+#临时文件存储点
 echo "socket=/tmp/mysql.sock" >>${myconfig}
 echo "user=mysql" >>${myconfig}
 echo "port=3306" >>${myconfig}
 echo "character-set-server=utf8" >>${myconfig}
+#跳过密码验证
 echo "skip-grant-tables" >>${myconfig}
 echo "symbolic-links=0" >>${myconfig}
 echo "[mysqld_safe]" >>${myconfig}
-echo "log-error=/var/log/mysqld.log" >>${myconfig}
-echo "pid-file=/var/run/mysqld/mysqld.pid" >>${myconfig}
+#这个路径在不同的Linuxn版本中有细微的差别 根据自己版本查看
+echo "log-error=/var/log/mariadb.log" >>${myconfig}
+echo "pid-file=/var/run/mariadb.pid" >>${myconfig}
+echo "!includedir /etc/my.cnf.d" >>${myconfig}
 #将mysql加入到服务，启动
+cd ${contents}/bin
+./mysqld --defaults-file=/etc/my.cnf --user=root
 cp ${contents}/support-files/mysql.server /etc/init.d/mysql
 chkconfig mysql on
 service mysqld start
@@ -54,10 +63,10 @@ source ${environment}
 #设置密码
 mysql -u root -p
 use mysql;
-update user set authentication_string=password\('t199628'\) where user='root';
+update user set authentication_string=password\('your password'\) where user='root';
 flush privileges;
-
-#设置远程连接
+#设置远程连接  有可能需要重新输入密码 具体原因不详
+alter user 'root'@'localhost' identified by'your password';
 update user set host='%' where user = 'root';
 flush privileges;
 exit
